fcf79972227f7c890654b3980a1bb4695ecc6c0e
Evgenii Fedoseev <abap@sapville.com>
2017-11-12 13:11:20 +0300

    add eslint jasmine support
    add a record to .eslintrs.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "browser": true,
         "es6": true,
         "node": true,
-        "jquery": true
+        "jquery": true,
+        "jasmine": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
